aff3a9bea06644c0cd70b80a19a6e38510ec2151
ofdiceandmagic <70441116+ofdiceandmagic@users.noreply.github.com>
2023-02-09 15:55:29 +0000

    add basic abcjs editor functionality

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 import $ from "jquery"
 import abcjs from "abcjs"
+
+$(function(){
+    let editor = new abcjs.Editor("editor",{
+        canvas_id: "tunes_container",
+        warnings_id: "abc-warnings"
+    })
+})
