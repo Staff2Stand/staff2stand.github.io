4e72ca56de1f6ffed6b8a0af95ba6faf953dff75
ofdiceandmagic <70441116+ofdiceandmagic@users.noreply.github.com>
2023-02-09 16:14:11 +0000

    jq

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-import $ from "jquery"
+// import $ from "jquery"
 import abcjs from "abcjs"
 
 $(function(){
+
+    /**
+     * INITIALIZE EDITOR
+     */
     let editor = new abcjs.Editor("editor",{
         canvas_id: "tunes_container",
         warnings_id: "abc-warnings"
