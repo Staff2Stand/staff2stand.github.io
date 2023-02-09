eaa63b8dbba09133986128e3c70126d50e60308f
ofdiceandmagic <70441116+ofdiceandmagic@users.noreply.github.com>
2023-02-09 16:20:22 +0000

    added abcjs opts

--- main.js
+++ main.js
@@ -2,12 +2,20 @@
 import abcjs from "abcjs"
 
 $(function(){
+    /**
+     * ABCJS RENDER OPTIONS
+     */
+    const abcjsOptions = {
+        add_classes:true,
+        responsive: 'resize'
+    }
 
     /**
      * INITIALIZE EDITOR
      */
     let editor = new abcjs.Editor("editor",{
         canvas_id: "tunes_container",
-        warnings_id: "abc-warnings"
+        warnings_id: "abc-warnings",
+        abcjsParams: abcjsOptions
     })
 })
