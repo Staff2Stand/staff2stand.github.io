08cc1dca2dd5ba29f35601b4baa154460e1594c4
ofdiceandmagic <70441116+ofdiceandmagic@users.noreply.github.com>
2023-03-29 12:09:27 +0000

    fix clear active class on bkmks on clear scores

--- main.js
+++ main.js
@@ -377,7 +377,7 @@ $(function(){
     //clear scores
     $("#clear_scores").click(function () {
         $('.abcEditor').each((i,editor) => $(editor).val('').change())
-        $(".score_bookmark.current").removeClass("active")
+        $(".score_bookmark.active").removeClass("active")
     })
     //toggle editors (Edit Button)
     $('#show_editors').click(function(){        
