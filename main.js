8528aef66bd2af35c6b529db6e1d379d64722ba9
Staff2Stand <129394480+Staff2Stand@users.noreply.github.com>
2023-05-02 11:07:42 -0400

    remove bkmk utils

--- main.js
+++ main.js
@@ -714,28 +714,6 @@ $(function(){
         }
     }
 
-    /**
-     * CREATE MY SCORES BOOKMARK
-     * @param {Object} scoreData Key is bookmark html attr. Value is the attr's value.
-     */
-    function createMyScoreBkmk(scoreData){
-        const $myScores = $('#myScores')
-        const faDeleteSVG = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg>`
-        const faMoveSVG = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>`
-
-        let bkmkHTML = `<li class="score_bookmark"`
-            for (const prop in scoreData){  bkmkHTML += ` ${prop}="${scoreData[prop]}"`  }
-        bkmkHTML += `></li>`
-        bkmkHTML += `<div class="bkmk-utils">`
-            bkmkHTML += `<span class="bkmk-util deleteBkmk">${faDeleteSVG}</span>`
-            bkmkHTML += `<span class="bkmk-util moveBkmk">${faMoveSVG}</span>`
-        bkmkHTML += `</div>`
-
-        const $bkmkEl = $(bkmkHTML)
-        $myScores.append($bkmkEl)
-        $myScores.sortable('refresh') //refresh sortable so the bkmk is recognized
-    }
-
     // FILE INPUT CHANGE
     //when the file input changes (ie: user selects a file)
     $('#loadScores').on("change", function() {
@@ -778,6 +756,39 @@ $(function(){
                 $('#loadScores').click()
             })
         }
+    })
+
+
+    /**
+     * CREATE MY SCORES BOOKMARK
+     * @param {Object} scoreData Key is bookmark html attr. Value is the attr's value.
+     */
+    function createMyScoreBkmk(scoreData){
+        const $myScores = $('#myScores')
+        const faDeleteSVG = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg>`
+        const faMoveSVG = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>`
+
+        let bkmkHTML = `<li class="score_bookmark"`
+            for (const prop in scoreData){  bkmkHTML += ` ${prop}="${scoreData[prop]}"`  }
+        bkmkHTML += `></li>`
+
+        const $bkmkEl = $(bkmkHTML)
+        $myScores.append($bkmkEl)
+        $myScores.sortable('refresh') //refresh sortable so the bkmk is recognized
+    }
+
+    /**
+     * MAKE MY SCORES SORTABLE
+     */
+    $('#myScores').sortable({
+        stop: function(event, ui) {
+            //Prevent click event after dragging the bkmk
+                //This works because "one-listeners" are fired before "normal" listeners. So if a one-listener stops propagation, it will never reach your previously set listeners.
+            $( event.originalEvent.target ).one('click', function(e){ e.stopImmediatePropagation(); } );
+        },
+        items: '> li', //only allow li children to be sortable (so, not the h1 section title),
+        scrollSensitivity: 10,
+        cursor: 'move'
     })
 
 
@@ -1113,21 +1124,6 @@ $(function(){
      * MAKE NOTEY DRAGGABLE
      */
     $('#notey .notey').draggable()
-
-    /**
-     * MAKE MY SCORES SORTABLE
-     */
-    $('#myScores').sortable({
-        stop: function(event, ui) {
-            //Prevent click event after dragging the bkmk
-                //This works because "one-listeners" are fired before "normal" listeners. So if a one-listener stops propagation, it will never reach your previously set listeners.
-            $( event.originalEvent.target ).one('click', function(e){ e.stopImmediatePropagation(); } );
-        },
-        items: '> li', //only allow li children to be sortable (so, not the h1 section title),
-        scrollSensitivity: 10,
-        cursor: 'move'
-    })
-
 
     /**
      * ESCAPE AND UNESCAPE ABC STRINGS
