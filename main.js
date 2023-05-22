7a774bee84d8a99ee52e0773ccf4c044cf03f134
Staff2Stand <129394480+Staff2Stand@users.noreply.github.com>
2023-05-22 15:22:09 -0400

    testing line spacing issue for bass

--- main.js
+++ main.js
@@ -518,6 +518,8 @@ $(function(){
         })
 
         //Adjust viewBox of each line's svg to show notenames and fingerings
+        const staffHeight = $(abcContainer).find('.abcjs-staff').get(0).getBBox().height
+        
         const $lineDivs = $(abcContainer).children('div')
         const $lineSVGs = $lineDivs.children('svg')
         $lineSVGs.each((i,svg)=>{
