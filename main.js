61d418e899580cc75a0d9c3f55fee183d1aa26e2
Staff2Stand <129394480+Staff2Stand@users.noreply.github.com>
2023-05-25 08:48:00 -0400

    my score saving testing

--- main.js
+++ main.js
@@ -357,11 +357,13 @@ $(function(){
                     //This means the old editor val is ''.
                     //  This occurs when a bkmk is clicked (not shift-clicked)
                     //      - ie, the renderScoreFromBkmk function clears all of the editors first
+                    console.log('not saved')
                     return
                 }
                 if (!eachOldBkmkAbc){
                     //This means there was a value in the editor, but it wasn't abc notation
                     //  When the user shift-clicks to append a score, editors with no value will have '\n' appended
+                    console.log('not saved')
                     return
                 }
                 if (!newAbc) {
@@ -370,6 +372,7 @@ $(function(){
                     //   or bc the bkmk didn't have an abc attr for this instrument.
                     //  Either way we need to set this instrument attr on the bkmk(s) to ''
                     $(`#myScores .score_bookmark.active`).attr(`abc-${instrument}`,'')
+                    console.log('not saved')
                     return
                 }
                 let saveSuccessful = false
@@ -380,12 +383,14 @@ $(function(){
                         //If oldBkmkAbc is undefined, the user shift-clicked to append a bkmk
                         //  (bc there are now more newBkmkAbc than oldBkmkAbc).
                         //  We return so that the new abc val isn't saved to the old bkmk
+                        console.log('not saved')
                         return
                     }
                     const $bkmk = S2S.activeScores[i]
                     const numChanges = $bkmk.attr('numChanges')
                     if (numChanges == 0){
                         //It was just clicked. Return so the old abc val isn't saved to the new bkmk
+                        console.log('not saved')
                         return
                     } else {
                         $bkmk.attr('numChanges', numChanges+1)
